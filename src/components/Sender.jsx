3365f6d5c7a1cb111336c8c45ea88015efa17d8e
vmhowley <vmhowleyh@gmail.com>
2023-09-27 11:05:09 -0400

    v0.0.1

---
+++ src/components/Sender.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react';
+
+export default   function MyForm(props) {
+    const [inputs, setInputs] = useState({});
+  
+    const handleChange = (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setInputs(values => ({...values, [name]: value}))
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(inputs);
+      props.setCurrentStep(props.currentStep + 1)
+      
+    }
+    return(
+    <div className='flex justify-center mt-2' >
+    <div className="w-full max-w-md">
+      <form onSubmit={handleSubmit} className="rounded-lg px-6 pt-2 pb-8 mb-4">
+        <div className="mb-4">
+          <label
+            className="block font-bold mb-2"
+            htmlFor="name"
+          >
+            Sender Name
+          </label>
+          <input
+            className="shadow appearance-none  rounded-lg w-full py-4 px-3    leading-tight focus:outline-none focus:shadow-outline bg-opacity-70 bg-gray-700"
+            id="name"
+            name='name'
+            type="text"
+            placeholder="Jhon Smith"
+          />
+        </div>
+        
+        
+        <div className="flex items-center justify-center">
+          <button onClick={setInputs}
+            disabled={props.currentStep === 0 ? true : false}
+            className="bg-emerald-400 hover:bg-emerald-300 text-white font-bold py-4 px-4 rounded-full focus:outline-none focus:shadow-outline w-full"
+            type="submit"
+            >
+            Continue
+          </button>
+         
+        </div>
+      </form>
+    </div>
+    </div>
+    )
+}
--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useState } from 'react';
+import Sender from './Sender';
 
-export default function Stepper() {
+
+
+export default function Stepper(step, setCurrStep) {
     const steps = ["Sender", "Receiver", "Package", "Payment", "Finish"];
     const [currentStep, setCurrentStep] = useState(1);
+
+    {currentStep === 1 && <Sender setCurrentStep={setCurrentStep} />}
     return (
+
       <div className=' divide-y divide-slate-200  text-sm mb-10'>
         <div className="flex justify-between">
           {steps?.map((step, i) => (
@@ -16,94 +22,7 @@ export default function Stepper() {
               <p className="text-slate-500 text-xs ">{step}</p>
             </div>
           ))}
-        </div>
-        <div className='flex justify-center mt-2' >
-        <div className="w-full max-w-md">
-          <form className="rounded-lg px-6 pt-2 pb-8 mb-4">
-            <div className="mb-4">
-              <label
-                className="block font-bold mb-2"
-                htmlFor="name"
-              >
-                Sender Name
-              </label>
-              <input
-                className="shadow appearance-none  rounded-lg w-full py-4 px-3    leading-tight focus:outline-none focus:shadow-outline bg-opacity-70 bg-gray-700"
-                id="name"
-                type="text"
-                placeholder="Jhon Smith"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block      font-bold mb-2"
-                htmlFor="phone"
-              >
-                Phone Number
-              </label>
-              <input
-                className="shadow appearance-none  rounded-lg w-full py-4 px-3    leading-tight focus:outline-none focus:shadow-outline bg-opacity-70 bg-gray-700"
-                id="phone"
-                type="number"
-                placeholder="809 402 XXXX"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block      font-bold mb-2"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <input
-                className="shadow appearance-none  rounded-lg w-full py-4 px-3    leading-tight focus:outline-none focus:shadow-outline bg-opacity-70 bg-gray-700"
-                id="email"
-                type="text"
-                placeholder="JhonSmith@delifast.com"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block      font-bold mb-2"
-                htmlFor="city"
-              >
-                City / Province
-              </label>
-              <input
-                className="shadow appearance-none  rounded-lg w-full py-4 px-3    leading-tight focus:outline-none focus:shadow-outline bg-opacity-70 bg-gray-700"
-                id="username"
-                type="text"
-                placeholder="Santo Domingo, Rep.Dom"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block      font-bold mb-2"
-                htmlFor="address"
-              >
-                Address Details
-              </label>
-              <input
-                className="shadow appearance-none  rounded-lg w-full py-4 px-3    leading-tight focus:outline-none focus:shadow-outline bg-opacity-70 bg-gray-700"
-                id="address"
-                type="text"
-                placeholder="C/ Francisco prats Ramirez..."
-              />
-            </div>
-            
-            <div className="flex items-center justify-center">
-              <button
-                className="bg-emerald-400 hover:bg-emerald-300 text-white font-bold py-4 px-4 rounded-full focus:outline-none focus:shadow-outline w-full"
-                type="button"
-              >
-                Continue
-              </button>
-             
-            </div>
-          </form>
-        </div>
-        </div>
-        
+        </div>       
 
         
       </div>
