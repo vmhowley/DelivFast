c24ba7f5b50b71f968c7daaece567c739498b1eb
vmhowley <vmhowleyh@gmail.com>
2023-09-29 18:11:51 -0400

    m

--- src/routes/MakeOrder.jsx
+++ src/routes/MakeOrder.jsx
@@ -13,7 +13,38 @@ export default function MakeOrder() {
         <span className='font-bold '>Back</span>
       </div>
       <Stepper />
-      <OrderForm />
+      <div className='flex justify-center mt-2' >
+    <div className="w-full max-w-md">
+      <form  className="rounded-lg px-6 pt-2 pb-8 mb-4">
+        <div className="mb-4">
+          <label
+            className="block font-bold mb-2"
+            htmlFor="name"
+          >
+            Sender Name
+          </label>
+          <input
+            className="shadow appearance-none  rounded-lg w-full py-4 px-3    leading-tight focus:outline-none focus:shadow-outline bg-opacity-70 bg-gray-700"
+            id="name"
+            name='name'
+            type="text"
+            placeholder="Jhon Smith"
+          />
+        </div>
+        
+        
+        <div className="flex items-center justify-center">
+          <button 
+            className="bg-emerald-400 hover:bg-emerald-300 text-white font-bold py-4 px-4 rounded-full focus:outline-none focus:shadow-outline w-full"
+            type="submit"
+            >
+            Continue
+          </button>
+         
+        </div>
+      </form>
+    </div>
+    </div>
     </>
   );
 }
