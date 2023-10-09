43aa33428bda6e11489cfc50c2b33a987aeeca10
vmhowley <vmhowleyh@gmail.com>
2023-10-09 12:08:06 -0400

    last

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithRedirect, getRedirectResult } from "firebase/auth";
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,25 +22,31 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
-export const auth = getAuth(app);
+
+
+export const auth = getAuth();
+
 export const db = getDatabase(app);
 
 //sign with google popup
-export const signInWithGoogle = () => {
-  signInWithPopup(auth, provider)
-   .then((result) => {
-    const name = result.user.displayName;
-    const email = result.user.email;
-    const profilePic = result.user.photoURL;
-     console.log(result);
-     localStorage.setItem('authenticated', true);
-     localStorage.setItem('name', name);
-     localStorage.setItem('email', email);
-     localStorage.setItem('profilePic', profilePic);
-     window.location.reload();
-  })
-  .catch((error) => {
-   alert(error);
-  })
+export const signInWithGoogle = async () => {
+  
+  const provider = new GoogleAuthProvider();
+  await signInWithRedirect(auth, provider);
+ 
  };
+ getRedirectResult(auth)
+  .then((result) => {
+   const name = result.user.displayName;
+   const email = result.user.email;
+   const profilePic = result.user.photoURL;
+    console.log(result);
+    localStorage.setItem('authenticated', true);
+    localStorage.setItem('name', name);
+    localStorage.setItem('email', email);
+    localStorage.setItem('profilePic', profilePic);
+    window.location.reload();
+ })
+ .catch((error) => {
+  console.log(error);
+ })
