ec4ad98f5cfa0597e13e092534ed2a41073023fc
vmhowley <vmhowleyh@gmail.com>
2023-10-17 22:05:01 -0400

    pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { PencilSquareIcon, CurrencyDollarIcon, MapPinIcon, QuestionMarkCircleIcon } from '@heroicons/react/24/solid'
+import { Link } from "react-router-dom";
 
 
 export default function Hero   () {
@@ -7,19 +8,21 @@ export default function Hero   () {
 
   return (
  <div className='flex flex-wrap m-auto gap-4 w-96  font-semibold'>
-      <a href='/create-order' className='transition-transform transform hover:scale-110  border h-[126px] w-[183px] grid justify-items-center content-center items-center rounded-2xl bg-opacity-30'>
+        <Link to='create-order' className='transition-transform transform hover:scale-110  border h-[126px] w-[183px] grid justify-items-center content-center items-center rounded-2xl bg-opacity-30'>
         <div className='p-4 bg-gray-100  rounded-full'>
         <PencilSquareIcon className=' text-emerald-400 w-[28px]' />
         </div>
           <span className=''>Make Order</span>
-      </a>
+      </Link>
 
-      <a href='/checkrates' className='card transition-transform transform hover:scale-110  border h-[126px] w-[183px] grid justify-items-center items-center rounded-2xl content-center'>
+      <Link to='check_rate' className='transition-transform transform hover:scale-110  border h-[126px] w-[183px] grid justify-items-center content-center items-center rounded-2xl bg-opacity-30'>
+
         <div className='p-4 bg-gray-100 rounded-full'>
         <CurrencyDollarIcon className='relative text-emerald-400 rounded-full w-[28px]' />
         </div>
           <span className=''>Check Rates</span>
-      </a>
+      </Link>
+      
 
       <a href='/nearbydrops' className='card transition-transform transform hover:scale-110  border h-[126px] w-[183px] grid justify-items-center items-center rounded-2xl bg-opacity-30 content-center'>
         <div className='p-4 bg-gray-100 rounded-full'>
