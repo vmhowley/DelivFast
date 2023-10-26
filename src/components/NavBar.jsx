8d5a1497af67692d205795afeb6760c882f4974e
vmhowley <vmhowleyh@gmail.com>
2023-10-26 15:29:14 -0400

    Last Updatess

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ export default function NavBar() {
  
 
   return (
-<div className="p-2 dark:bg-opacity-60 bg-opacity-60  fixed flex  justify-around text-center content-center bottom-0  w-full text-emerald-400  font-semibold bg-white dark:bg-zinc-900   backdrop-blur-md text-xs ">
+<div className="p-2 dark:bg-opacity-60 bg-opacity-60  fixed flex  justify-around text-center content-center bottom-0  w-full text-emerald-400  font-semibold bg-white dark:bg-zinc-900   backdrop-blur-md text-xs z-50 ">
         <Link className="flex flex-col content-center items-center justify-between" to="/">
             <HomeIcon className="h-6" />
             Home
